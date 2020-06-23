beb209263006e6ce5b8d8226ad592bdf3042eafb
Jacob Squier <jacob.squier@gmail.com>
2020-06-23 00:47:44 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
   count  = 0
-  found = nil
   while  count < array.length do
     if array[count] == value_to_find do
-      found = count
+      return count
     end
     count += 1
   end
-  return found
 end
